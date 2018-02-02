959baf6eb58dafb46bfb0414bb86c7ccf7f758a1
Roger Graham <roger.graham.1393@gmail.com>
2018-02-02 17:38:09 -0500

    Filter unparseable characters.

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ function encodeEntities(result, value, key) {
     result[key] = value;
     return;
   } else if (typeof value === 'string') {
+    value = value.replace(/[\u0000\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g, '');
     result[key] = _.escape(value);
     return;
   } else if (typeof value === 'object') {
